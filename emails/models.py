02b3dabdaedf0c97fdd677a8fa748a8153e20866
vasudeveloper001 <vasudeveloper001@gmail.com>
2020-09-02 22:49:20 +0530

    Updated EmailEntry model

--- emails/models.py
+++ emails/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class EmailEntry(models.Model):
     email = models.EmailField()
+    name = models.CharField(max_length=120, blank=True)
+    bio = models.TextField(blank=True)
+    updated = models.DateTimeField(auto_now=True) # set when saved
+    timestamp = models.DateTimeField(auto_now_add=True) # set when added
